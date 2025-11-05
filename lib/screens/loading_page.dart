682b5c6b157700d6c3f2d8f9cc254539d9c2a2fc
Chaizee <zuzindima26@gmail.com>
2025-11-05 20:56:35 +0600

    init loading page branch

---
+++ lib/screens/loading_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPage();
+}
+
+class _LoadingPage extends State<LoadingPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+    );
+  }
+}
